61115e220538505be2284c703762a2b16d981592
Upendra <tripathiupendra4321@gmail.com>
2026-03-23 16:50:13 +0530

    Task_PY200_T2 completed

---
+++ PythonPractice/Pythontask/Task_PY200_T2.py
@@ -0,0 +1,9 @@
+number=float(input("Enter a number"))
+
+# condition to check number is positive,negative or zero
+if number>0:
+    print(f"Your enter number {number} was positive")
+elif number<0:
+    print(f"Your enter number {number} was negative")
+else:
+    print(f"Your enter number {number} was zero")
